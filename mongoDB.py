68db5109f41fc1fe2b17b23f366c986486c904bd
edytaBr <ebredla@gmail.com>
2022-03-08 23:37:28 +0100

    update mongoDB.py - use .env

--- mongoDB.py
+++ mongoDB.py
@@ -7,12 +7,18 @@ Created on Thu Mar  3 14:38:43 2022
 """
 
 from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
-
+myclient_token = os.environ.get("myclient")
 
 def db(data, score1, score2, players):
-    myclient = MongoClient("")
+    myclient = MongoClient(myclient_token)
     mydb = myclient["mydatabase"]
     mycol = mydb["results"]
     mydict = {"Result": data, "Score Read Team": score1, "Score Blue Team": score2, "Players": str(players)}
     x = mycol.insert_one(mydict)
+
+
+
